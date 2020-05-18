15beefe38af23db1054f5b2666c9e0752a55e4ec
werkaflute <63801151+werkaflute@users.noreply.github.com>
2020-05-18 23:07:33 +0200

    Update WebWrapper.py

--- WebWrapper.py
+++ WebWrapper.py
@@ -8,15 +8,14 @@ app.config['ASSETS'] = os.path.join(app.root_path, 'assets')
 
 @app.route('/')
 def index():
-    """Strona główna wizualizacji/index.
-    Tu będzie tworzony graf.
-    """
+    # Strona główna wizualizacji, w której będzie tworzony graf
+
     return render_template('base.html')
 
 @app.route("/pdf/<filename>")
 def iframe(filename):
-    """Wyświetla wybrany plik.
-    """
+    # Wyświetlanie wybranego pliku
+
     return send_from_directory(app.config['ASSETS'], filename)
 
 @app.route("/load/graph")
@@ -24,6 +23,6 @@ def graph():
     return render_template('graph.js')
 
 def run():
-    """Włącza serwer http Flaskna porcie 80.
-    """
+    # Włącza serwer http Flask na porcie 80
+    
     app.run(port = 80)
